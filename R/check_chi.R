f6bb8f8dd1952515ddb512efbf0429ebb7e77f54
James McMahon <james.mcmahon@phs.scot>
2025-10-31 12:43:39 +0000

    Document and export function

--- R/check_chi.R
+++ R/check_chi.R
@@ -1,3 +1,9 @@
+#' Check a chi number
+#'
+#' @param chi_number A CHI
+#'
+#' @return TRUE or FALSE
+#' @export
 check_chi <- function(chi_number) {
   valid_day <- substr(chi_number, 1, 2) <= 31
   valid_month <- substr(chi_number, 3, 4) <= 12
